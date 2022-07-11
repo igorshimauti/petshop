a145a3e823ea483227ba6cb181866d6969a3ea31
Elias Andre Da Silva <87732458+Elias-WTS@users.noreply.github.com>
2022-07-11 20:10:34 -0300

    Add metodo adicionar cliente

--- src/main/java/br/com/projetocurso/petshop/controller/ClienteController.java
+++ src/main/java/br/com/projetocurso/petshop/controller/ClienteController.java
@@ -5,10 +5,7 @@ import br.com.projetocurso.petshop.service.ClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 
@@ -24,5 +21,11 @@ public class ClienteController {
         Cliente clienteIncluido = clienteService.salvar(cliente);
         URI localizacao = URI.create("cliente/" + clienteIncluido.getId());
         return ResponseEntity.created(localizacao).body(clienteIncluido);
+    }
+    @PutMapping(value = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Cliente> atualizar(@PathVariable Long id, @RequestBody Cliente cliente) {
+        cliente.setId(id);
+        Cliente clienteAtualizado = clienteService.salvar(cliente);
+        return ResponseEntity.ok().body(clienteAtualizado);
     }
 }
